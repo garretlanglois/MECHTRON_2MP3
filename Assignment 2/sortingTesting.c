422d3d312c1f532eb104dbf5c874884073d37283
Garret Langlois <gaslanglois@gmail.com>
2024-10-13 15:21:43 -0400

    Finished merge sort

--- Assignment 2/sortingTesting.c
+++ Assignment 2/sortingTesting.c
@@ -58,12 +58,83 @@ void mergeSort(int arr[], int n) {
 }
 */
 
-void mergeElement (int left, int right, int arr[]) {
+void mergeElements(int arr[], int leftPosition, int rightPosition) {
+
+    //It is important to do this instead of n/2 because the position in the original array dynamically
+    //changes throughout the recursion
+    int middlePosition = leftPosition + (rightPosition-leftPosition)/2;
+    
+    int leftSize = middlePosition - leftPosition + 1;
+    int rightSize = rightPosition - middlePosition;
+
+    //Now we need two different arrays which we will use to temporarly store the data
+
+    int leftArray[leftSize];
+    int rightArray[rightSize];
+
+
+    //Append the values of the array from the left side to the temporary array
+    for (int i = 0; i < leftSize; i++) {
+        leftArray[i] = arr[leftPosition + i ];
+    }
+
+    //Append the values of the array from the right side to the temporary array
+    for (int i = 0; i < rightSize; i++) {
+        rightArray[i] = arr[1 + middlePosition + i];
+    }
+
+    //Had to ask ChatGPT how to get an extra two indexing variables.
+    //It ended up being only these two extra lines that I needed.
+    //The reason that they are both set to 0 is because i was already used
+    int i = 0; //This is the indexing variable for the left array
+    int j = 0; //This is the indexing variable for the right array
+    int k; //This is the whole array index
+    int iterationNum = (rightSize+leftSize)/2 + 1;
+
+    //Now we need to add the values into the original array, which remember is still in the recursive loop
+    //so it isnt actually the original array we were sorting
+    // Total number of elements in the range to merge is from leftPosition to rightPosition
+    for (k = leftPosition; k <= rightPosition; k++) {
+        if (i < leftSize && (j >= rightSize || leftArray[i] <= rightArray[j])) {
+            arr[k] = leftArray[i];
+            i++; // Move index in leftArray
+        } else {
+            arr[k] = rightArray[j];
+            j++; // Move index in rightArray
+        }
+    }
+
+
+    //Now we just need to copy over any remaining elements that were not included in the loop.
+    //the reason that this happens is because of the bound we set with the division
+
+    //This copy strategy was taken from this article: https://www.geeksforgeeks.org/c-program-for-merge-sort/
+
+    while (i < leftSize) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j < rightSize) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
 
 }
 
-void merge() {
-    
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r-l)/2;
+
+        mergeSort(arr, l, m);
+
+        mergeSort(arr, m+1,r);
+
+        mergeElements(arr, l , r);
+
+    }
 }
 
 
@@ -106,7 +177,7 @@ int main() {
     printf("Original array: ");
     printArray(testArr, n);
     //bubbleSort(testArr, n);
-    mergeSort(testArr, n);
+    mergeSort(testArr, 0, n-1);
     printf("Bubble sorted array: ");
     printArray(testArr, n);
     
