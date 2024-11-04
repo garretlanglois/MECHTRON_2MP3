bbd190283eeab77428d89fef29569d97a12bf29b
Garret Langlois <gaslanglois@gmail.com>
2024-11-04 10:34:59 -0500

    Added 3d dynamic memory allocation

--- Quiz Prep/Quiz 5/dynamicMemAlloc.c
+++ Quiz Prep/Quiz 5/dynamicMemAlloc.c
@@ -24,6 +24,38 @@ int main(void) {
 
     //3D dynamic memory allocation
 
+    //matrix properties
+    int layers = 3;
+    int rows_per_layer[] = {2, 3, 2}; // Number of rows in each layer
+    int cols_per_row[3][3] = {
+        {4, 5},      // Layer 0 has 2 rows with 4 and 5 columns
+        {3, 2, 6},   // Layer 1 has 3 rows with 3, 2, and 6 columns
+        {5, 3}       // Layer 2 has 2 rows with 5 and 3 columns
+    };
+
+
+    int ***raggedThree = malloc(layers*sizeof(int**));
+
+    for (int i = 0; i < layers; i++) {
+        raggedThree[i] = malloc(rows_per_layer[i]*sizeof(int*));
+        for (int j = 0; j < rows_per_layer[i]; j ++) {
+            raggedThree[i][j] = malloc(cols_per_row[i][j]*sizeof(int));
+        }
+    }
+
+
+    //GPT generated print array function
+    for (int i = 0; i < layers; i++) {
+        printf("Layer %d:\n", i);
+        for (int j = 0; j < rows_per_layer[i]; j++) {
+            for (int k = 0; k < cols_per_row[i][j]; k++) {
+                raggedThree[i][j][k] = i * 100 + j * 10 + k; // Example initialization
+                printf("ragged_array[%d][%d][%d] = %d\n", i, j, k, raggedThree[i][j][k]);
+            }
+        }
+        printf("\n");
+    }
+
     
 
 
