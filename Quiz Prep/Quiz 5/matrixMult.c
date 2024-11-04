855caa409639350bea5b16dc56ce2f8053c9c1e1
Garret Langlois <gaslanglois@gmail.com>
2024-11-04 11:12:14 -0500

    2d matrix multiplication

--- Quiz Prep/Quiz 5/matrixMult.c
+++ Quiz Prep/Quiz 5/matrixMult.c
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 
 
-void matrixMult(int m, int n, int p, int A[m][n], int B[n][p]) {
-    for (int i = 0; i < ) {
-
+void matrixMult(int m, int n, int p, int A[m][n], int B[n][p], int **C) {
+    for (int i = 0; i < m; i++) { //Iterate through the rows 
+        for (int j = 0; j < p; j++) { //Iterate through the columns in the other array (we are creating m x p)
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++) { //n is the common variable for both
+                C[i][j] += A[i][k]*B[k][j];
+            }
+        }
     }
 }
 
@@ -15,15 +20,40 @@ int main(void) {
     const int n = 3;
     const int p = 2;
 
-    int **A = malloc(m * sizeof(int*));
-    int **B = malloc(n * sizeof(int*));
-
+    //allocated rows
     int **C = malloc(m*sizeof(int*));
+
+    if (C == NULL) {
+        printf("The memory allocation failed");
+        free(C);
+    }
+
+    for (int i = 0; i < m; i++) {
+        C[i] = malloc(p*sizeof(int));
+    }
 
     int A[m][n] = {{1, 5, 3},{2, 6, 3}};
 
     int B[n][p] = {{1, 5},{9, 2},{7, 1}};
 
+    matrixMult(m, n, p, A, B, C);
 
+
+    //Print out the matrix
+    printf("Resultant matrix C:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    //Properly free memory
+    for (int i = 0; i < m; i++) {
+        free(C[i]);
+    }
+
+    free(C);
 
 }
