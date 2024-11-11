af31555654212b48ae047240958986dd20e9d9db
Garret Langlois <gaslanglois@gmail.com>
2024-11-10 19:24:20 -0500

    almost complete code

--- Assignment 3/vaderSentiment.c
+++ Assignment 3/vaderSentiment.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <math.h>
 
+//Values that can be adjusted
 #define ARRAY_SIZE 10
 #define MAX_STRING_LENGTH 17
-#define LEXICON_SIZE 7520
+#define LEXICON_SIZE 7525
+#define boostFactor 0.293
+
+#define positiveCount 11
+#define negativeCount 9
+#define negationCount 13
+
+char* positiveIntensifiers[positiveCount] = {"absolutely", "completely", "extremely", "really", "so", "totally", "very", "particulary", "exceptionally", 
+"incredibly", "remarkably"};
+
+char* negativeIntensifiers[negativeCount] = {"barely", "hardly", "scarcely", "somewhat", "mildly", "slightly", "partially",
+"fairly", "pretty much"};
+
+char* negations[negationCount] = {"not", "isn't", "doesn't", "wasn't", "shouldn't", "won't", "cannot", "can't",
+"nor", "neither", "without", "lack", "missing"};
 
 typedef struct {
     char word[MAX_STRING_LENGTH];
@@ -15,6 +30,7 @@ typedef struct {
 } WordData;
 
 WordData* lexicon[LEXICON_SIZE];
+
 
 void parseLexicon() {
     FILE *file = fopen("vader_lexicon.txt", "r");
@@ -185,15 +201,58 @@ char** tokenization(char * sentence, int size, int* tokenCount) {
 
 }
 
-int sentimentCalculation(char* word) {
+//This function needs to be updated to account for punctuation, caps, negation
+double sentimentCalculation(char* testWord, double *intensifierScore ) {
 
-    int i = 0;
+    double wordScore = 0;
 
-    while (lexicon[i] != word && i < LEXICON_SIZE) {
+    for (int i = 0; i < LEXICON_SIZE; i++) {
+        if (lexicon[i]->word == testWord) {
 
-        
+            double sentimentValue = lexicon[i]->meanSentiment;
 
+            wordScore = *intensifierScore*sentimentValue + sentimentValue;
+
+            *intensifierScore = 0;
+
+            for (int j = 0; j < positiveCount; j++) {
+                if (lexicon[i]->word == positiveIntensifiers[j]) {
+                    *intensifierScore = 0.293;
+                }
+            }
+            if(*intensifierScore == 0) {
+                for (int k = 0; k < negativeCount; k++) {
+                    if (lexicon[i]->word == negativeIntensifiers[k]) {
+                        *intensifierScore = 0.293;
+                    }
+                }
+            }
+
+            return wordScore;
+        } 
+        else {
+            continue;
+        }
     }
+
+    //If the word was not found in the lexicon return 0
+    return 0;
+
+}
+
+double compoundSentimentScoreCalculation(char** tokens, int sentenceLength) {
+
+    double intensifierScore = 0;
+
+    double totalScore = 0;
+
+    for (int i = 0; i < sentenceLength; i++) {
+        totalScore += sentimentCalculation(tokens[i], &intensifierScore);
+    }
+
+    double compoundScore = totalScore/sqrt(totalScore*totalScore +15);
+
+    return compoundScore;
 
 }
 
@@ -204,13 +263,13 @@ int main(void) {
 
     int tokenCount = 0;
     
-    char sentence[] = {"This is a sentence"};
+    char sentence[] = {"This is a sentence?!"};
 
     char** tokens = tokenization(sentence, 18, &tokenCount);
 
     int lexiconSize = sizeof(lexicon)/sizeof(WordData *);
 
-    printf("\n\nThe value is %s\n", lexicon[7519]->word);
+    printf("\n\nThe value is %s\n", lexicon[7524]->word);
 
     printf("\nThe lexicon size is: %d", lexiconSize);
 
