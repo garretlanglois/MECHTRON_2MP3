e9bff04533c62375b19abb86f191158df7cc2052
Garret Langlois <gaslanglois@gmail.com>
2024-11-25 12:27:38 -0500

    quizPractice

---
+++ Quiz Prep/Quiz 6/hashPractice.c
@@ -0,0 +1,58 @@
+//Hash table with linkedList for collisions!
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE 20
+
+typedef struct HashEntry { 
+    int value;
+    int key;
+    struct HashEntry* next;
+} HashEntry;
+
+HashEntry* hashTable[SIZE];
+
+int hash(int value) {
+    return value % 20;
+}
+
+void addValue(int value) {
+
+    int hashedPosition = hash(value);
+
+    HashEntry* tableValue = hashTable[hashedPosition];
+
+    if (tableValue != NULL) {
+
+        HashEntry* tmp = hashTable[hashedPosition]->next;
+
+        HashEntry* previous = NULL;
+
+        while (tmp != NULL) {
+            previous = tmp;
+            tmp = tmp->next;
+        }
+
+        tmp->next = malloc(sizeof(struct HashEntry));
+
+        tmp->next->value = value;
+        tmp->next->next = NULL;
+    }
+    else {
+        tableValue->next = NULL;
+        tableValue->value = value;
+    }
+
+}
+
+int main(void) {
+
+    int value = 63;
+    addValue(value);
+
+    int hashedPosition = hash(value);
+
+    printf("The value at the hashed position is %d\n", hashTable[hashedPosition]->value);
+
+}
