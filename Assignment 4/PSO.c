0962e39750b426896db71d71dafa2880dbe33030
Garret Langlois <gaslanglois@gmail.com>
2024-11-30 14:23:11 -0500

    created initial file setups

---
+++ Assignment 4/PSO.c
@@ -0,0 +1,15 @@
+// CODE: include library(s)
+#include "utility.h";
+
+
+// Helper function to generate random numbers in a range
+double random_double(double min, double max) {
+    return min + (max - min) * ((double)rand() / RAND_MAX);
+}
+
+// CODE: implement other functions here if necessary
+
+double pso(ObjectiveFunction objective_function, int NUM_VARIABLES, Bound *bounds, int NUM_PARTICLES, int MAX_ITERATIONS, double *best_position) {
+
+    // CODE: implement pso function here
+}
